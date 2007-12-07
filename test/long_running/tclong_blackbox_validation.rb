d0af8c9baa58168c05917e08935de6ddc32726ae
Brian Noll <brian.noll@openlogic.com>
2007-12-07 17:33:34 +0000

    cleaning this test up a bit - no functional difference
    git-svn-id: svn://svn.code.sf.net/p/ossdiscovery/code/trunk/app@36 fd6d9157-2c40-0410-9642-e7077f30ef04

--- test/long_running/tclong_blackbox_validation.rb
+++ test/long_running/tclong_blackbox_validation.rb
@@ -11,6 +11,9 @@ class TclongBlackboxValidation < Test::Unit::TestCase
   
   @@log = Config.log
   
+  DIR_TO_DISCOVER = File.expand_path(File.join(File.dirname(__FILE__), '..', 'resources')) unless defined? DIR_TO_DISCOVER
+  DISCOVERY_RB = File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'main', 'lib', 'discovery.rb')) unless defined? DISCOVERY_RB
+  
   def setup
     
   end
@@ -22,15 +25,12 @@ class TclongBlackboxValidation < Test::Unit::TestCase
   def test_discovery
     t1 = Time.new
     @@log.info('TclongBlackboxValidation') {"Running... #{t1}"}
-    
-    dir_to_discover = File.expand_path(File.join(File.dirname(__FILE__), '..', 'resources'))
-    discovery_rb = File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'main', 'lib', 'discovery.rb'))
-    output = `ruby #{discovery_rb} --path #{dir_to_discover}`
-    assert(!output.include?('decoy'))
-    
+    output = `ruby #{DISCOVERY_RB} --path #{DIR_TO_DISCOVER}`
     t2 = Time.new
     @@log.info('TclongBlackboxValidation') {"It took '#{(t2-t1).to_s}' seconds to run the blackbox validation test."}
     
+    # We're testing that content that should not be discovered (content living somewhere underneath a 'decoy' directory) is actually NOT being discovered.
+    assert(!output.include?('decoy'))
   end
 
 end
