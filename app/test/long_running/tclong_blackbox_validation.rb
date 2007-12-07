f305bfffa36ceb8c8b678a285bc263340210e449
Brian Noll <brian.noll@openlogic.com>
2007-12-07 17:23:48 +0000

    renaming a file
    git-svn-id: svn://svn.code.sf.net/p/ossdiscovery/code/trunk@35 fd6d9157-2c40-0410-9642-e7077f30ef04

--- app/test/long_running/tclong_blackbox_validation.rb
+++ app/test/long_running/tclong_blackbox_validation.rb
@@ -6,7 +6,7 @@ require File.join(File.dirname(__FILE__), '..', 'test_helper')
 require File.join(File.dirname(__FILE__), 'discovery_assertions')
 require 'conf/config'
 
-class TclongDecoy < Test::Unit::TestCase
+class TclongBlackboxValidation < Test::Unit::TestCase
   include DiscoveryAssertions
   
   @@log = Config.log
@@ -21,7 +21,7 @@ class TclongDecoy < Test::Unit::TestCase
 
   def test_discovery
     t1 = Time.new
-    @@log.info('TclongDecoy') {"Running... #{t1}"}
+    @@log.info('TclongBlackboxValidation') {"Running... #{t1}"}
     
     dir_to_discover = File.expand_path(File.join(File.dirname(__FILE__), '..', 'resources'))
     discovery_rb = File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'main', 'lib', 'discovery.rb'))
@@ -29,7 +29,7 @@ class TclongDecoy < Test::Unit::TestCase
     assert(!output.include?('decoy'))
     
     t2 = Time.new
-    @@log.info('TclongDecoy') {"It took '#{(t2-t1).to_s}' seconds to run the decoy blackbox test."}
+    @@log.info('TclongBlackboxValidation') {"It took '#{(t2-t1).to_s}' seconds to run the blackbox validation test."}
     
   end
 
