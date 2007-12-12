7d7f193d95b62d2688e0ec2e4942271061f1fa09
Eric Weidner <eric.weidner@openlogic.com>
2007-12-12 00:34:59 +0000

    added legal notice
    git-svn-id: svn://svn.code.sf.net/p/ossdiscovery/code/trunk/app@60 fd6d9157-2c40-0410-9642-e7077f30ef04

--- main/discovery.bat
+++ main/discovery.bat
@@ -1,6 +1,32 @@
 @echo off
 @setlocal
 
+REM discovery.bat
+REM
+REM LEGAL NOTICE
+REM -------------
+REM 
+REM OSS Discovery is a tool that finds installed open source software.
+REM    Copyright (C) 2007 OpenLogic, Inc.
+REM  
+REM OSS Discovery is free software: you can redistribute it and/or modify
+REM it under the terms of the GNU Affero General Public License version 3 as 
+REM published by the Free Software Foundation.  
+REM  
+REM 
+REM This program is distributed in the hope that it will be useful,
+REM but WITHOUT ANY WARRANTY; without even the implied warranty of
+REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+REM GNU Affero General Public License version 3 (discovery2-client/license/OSSDiscoveryLicense.txt) 
+REM for more details.
+REM  
+REM You should have received a copy of the GNU Affero General Public License along with this program.  
+REM If not, see http://www.gnu.org/licenses/
+REM  
+REM You can learn more about OSSDiscovery, report bugs and get the latest versions at www.ossdiscovery.org.
+REM You can contact the OSS Discovery team at info@ossdiscovery.org.
+REM You can contact OpenLogic at info@openlogic.com.
+
 set DISCOVERY_HOME=%~dp0%
 
 cd "%DISCOVERY_HOME%"
