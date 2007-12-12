fec68573da1682f8d3cba40338ff33fe9091c768
Eric Weidner <eric.weidner@openlogic.com>
2007-12-12 00:06:16 +0000

    updated scripts to include path fixing code for windows and linux.  Did not fix paths for cygwin
    git-svn-id: svn://svn.code.sf.net/p/ossdiscovery/code/trunk@57 fd6d9157-2c40-0410-9642-e7077f30ef04

--- app/main/discovery.bat
+++ app/main/discovery.bat
@@ -1,3 +1,8 @@
+@echo off
+@setlocal
 
-ruby lib\discovery.rb --progress 100 --human-results readable_scanresults.txt %1 %2 %3 %4 %5 %6
+set DISCOVERY_HOME=%~dp0%
+
+cd "%DISCOVERY_HOME%"
+ruby %DISCOVERY_HOME%\lib\discovery.rb --progress 100 --human-results readable_scanresults.txt %1 %2 %3 %4 %5 %6
 
