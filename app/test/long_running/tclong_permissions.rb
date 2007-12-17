e1f93793bf3c501419c45b54c4fe17e8059c0715
Brian Noll <brian.noll@openlogic.com>
2007-12-17 20:21:21 +0000

    fixing permission tests so it doesn't break if it's being run as root
    git-svn-id: svn://svn.code.sf.net/p/ossdiscovery/code/trunk@78 fd6d9157-2c40-0410-9642-e7077f30ef04

--- app/test/long_running/tclong_permissions.rb
+++ app/test/long_running/tclong_permissions.rb
@@ -6,6 +6,8 @@ $:.unshift File.join(File.dirname(__FILE__), '..', '..', 'main', 'lib')
 require 'conf/config'
 
 class TclongPermissions < Test::Unit::TestCase
+  
+  @@log = Config.log
 
   DISCOVERY_RB = File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'main', 'lib', 'discovery.rb')) unless defined? DISCOVERY_RB
   PERMISSIONS_DIR_TO_DISCOVER = File.expand_path(File.join(File.dirname(__FILE__), '..', 'resources', 'content-cg', 'permission-tests')) unless defined? PERMISSIONS_DIR_TO_DISCOVER
@@ -17,10 +19,16 @@ class TclongPermissions < Test::Unit::TestCase
   end
   
   def test_permission_count
-    cmd = "ruby #{DISCOVERY_RB} --path #{PERMISSIONS_DIR_TO_DISCOVER}"
-    output = `#{cmd}`
-    pd_val = output.match(/^permission denied\s+:\s+(.*).*$/)[1]
-    assert_equal(2, pd_val.to_i, "Expected to be denied permission to two items (one directory and one file) in the '#{PERMISSIONS_DIR_TO_DISCOVER}' directory.")
+    whoami_result = `whoami`
+    if (whoami_result.include?('root')) then
+      assert true
+      @@log.warn('TclongPermissions'){'Unable to test the permissions functionality because this process is being run by the root user, which has permissions to everything.'}
+    else
+      cmd = "ruby #{DISCOVERY_RB} --path #{PERMISSIONS_DIR_TO_DISCOVER}"
+      output = `#{cmd}`
+      pd_val = output.match(/^permission denied\s+:\s+(.*).*$/)[1]
+      assert_equal(2, pd_val.to_i, "Expected to be denied permission to two items (one directory and one file) in the '#{PERMISSIONS_DIR_TO_DISCOVER}' directory.")
+    end
   end
 
 end
