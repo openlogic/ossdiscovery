f3460f68d0c58f95b34652322f6f2e592befb305
Rod Cope <rod.cope@openlogic.com>
2008-01-08 18:02:12 +0000

    Removed company_name from config.yml and uname-based machine_id calculation.
    git-svn-id: svn://svn.code.sf.net/p/ossdiscovery/code/trunk/app/main@138 fd6d9157-2c40-0410-9642-e7077f30ef04

--- lib/cliutils.rb
+++ lib/cliutils.rb
@@ -501,7 +501,7 @@ def make_machine_id
                              # if using JRuby
     make_simple_machine_id   
   else  # every other platform including cygwin supports uname -a
-    make_uname_based_machine_id( platform )
+    make_uname_based_machine_id platform
   end
 end
 
@@ -519,7 +519,7 @@ def make_simple_machine_id
   @machine_id = Digest::MD5.hexdigest(hostname + ipaddr + @distro)
 end
 
-def make_uname_based_machine_id( platform )
+def make_uname_based_machine_id(platform)
   #-------------- the uname method --------------------------------------------------------------------
   # this assumes all other platforms support uname -a
   # this also assumes that if a machine upgrades its OS or changes its hostname it essentially becomes
@@ -591,7 +591,7 @@ def make_uname_based_machine_id( platform )
   # obfuscate the entire blob of info we gather into a single, 32 byte MD5 sum   
   # if any single one of these items changes over time, the machine ID will change.  This is a known
   # limitation.
-  @machine_id = Digest::MD5.hexdigest( hostname + mac + @uname + @company_name + @distro )
+  @machine_id = Digest::MD5.hexdigest(hostname + mac + @uname + @distro)
 end
 
 =begin rdoc
