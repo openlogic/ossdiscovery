9e85225da6f4a0316e39bc994699f89ca211313b
Rod Hilton <rod.hilton@openlogic.com>
2008-02-05 03:57:49 +0000

    Added two new scripts to compliment the existing discovery and discovery.bat scripts.  These simply use jruby rather than native ruby.  Jruby itself is also added to the repo, as a flattened jar file under lib
    git-svn-id: svn://svn.code.sf.net/p/ossdiscovery/code/trunk/app/main@186 fd6d9157-2c40-0410-9642-e7077f30ef04

---
+++ discovery_jruby.bat
@@ -0,0 +1,34 @@
+@echo off
+@setlocal
+
+REM discovery.bat
+REM
+REM LEGAL NOTICE
+REM -------------
+REM 
+REM OSS Discovery is a tool that finds installed open source software.
+REM    Copyright (C) 2007 OpenLogic, Inc.
+REM  
+REM OSS Discovery is free software: you can redistribute it and/or modify
+REM it under the terms of the GNU Affero General Public License version 3 as 
+REM published by the Free Software Foundation.  
+REM  
+REM 
+REM This program is distributed in the hope that it will be useful,
+REM but WITHOUT ANY WARRANTY; without even the implied warranty of
+REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+REM GNU Affero General Public License version 3 (discovery2-client/license/OSSDiscoveryLicense.txt) 
+REM for more details.
+REM  
+REM You should have received a copy of the GNU Affero General Public License along with this program.  
+REM If not, see http://www.gnu.org/licenses/
+REM  
+REM You can learn more about OSSDiscovery, report bugs and get the latest versions at www.ossdiscovery.org.
+REM You can contact the OSS Discovery team at info@ossdiscovery.org.
+REM You can contact OpenLogic at info@openlogic.com.
+
+set DISCOVERY_HOME=%~dp0%
+
+cd "%DISCOVERY_HOME%"
+java -cp $DISCOVERY_HOME/lib/jruby-complete-1.0.1.jar org.jruby.Main %DISCOVERY_HOME%\lib\discovery.rb --progress 100 --human-results readable_scanresults.txt %1 %2 %3 %4 %5 %6
+
