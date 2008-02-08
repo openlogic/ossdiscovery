435b75cd53a040d913b770bd08621a4d473b620c
Brian Noll <brian.noll@openlogic.com>
2008-02-08 18:28:14 +0000

    done experimenting  - I hope/think - with the jruby CCrb stuff and am actually committing something that should make it all work
    git-svn-id: svn://svn.code.sf.net/p/ossdiscovery/code/trunk@208 fd6d9157-2c40-0410-9642-e7077f30ef04

--- app/test/test_helper.rb
+++ app/test/test_helper.rb
@@ -2,7 +2,7 @@ require 'find'
 require 'set'
 
 class TestHelper
-  
+
 =begin rdoc
   Returns the same thing that this unix command would return. 
 
@@ -11,14 +11,24 @@ class TestHelper
   The 'dir' arg is expected to be a fully qualified directory name.
   Specifically, it returns a Set of fully qualified filename Strings.
 =end  
-  def TestHelper.find_all_files(dir) 
+  def TestHelper.find_all_files(dir)
     all_files = Set.new
     Find.find(dir) do |path|
-      if (File.file?(path) && !path.include?(".svn")) then
-        all_files << File.expand_path(path)
+      if (File.basename(path) == 'resources') then
+        puts "pruning this directory: '#{path}'"
+        # The resources directory contains the symlink test directory which intentionally contains
+        # is infinitely recursive circular symlink. If jruby is running this code (as opposed to
+        # native ruby) then the process goes out to lunch forever.  In addition to this, the 'resources'
+        # directory contains no test cases, only resources used by those test cases, so it is safe
+        # to exclude it in this case.
+        Find.prune
+      else
+        if (File.file?(path) && !path.include?(".svn")) then
+          all_files << File.expand_path(path)
+        end
       end
-    end
+
     return all_files
   end
-  
+
 end
