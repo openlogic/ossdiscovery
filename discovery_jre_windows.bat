d3564a9f478d0954feb19c2377ad5c82d2a42f7b
Rod Hilton <rod.hilton@openlogic.com>
2008-02-11 21:46:36 +0000

    checking in some bundleable jres to create distributions that don't require java for discovery to run.  also modified rakefile to build these distributions
    git-svn-id: svn://svn.code.sf.net/p/ossdiscovery/code/trunk/app/main@222 fd6d9157-2c40-0410-9642-e7077f30ef04

---
+++ discovery_jre_windows.bat
@@ -0,0 +1,35 @@
+@echo off
+@setlocal
+
+REM discovery.bat
+REM
+REM LEGAL NOTICE
+REM -------------
+REM 
+REM OSS Discovery is a tool that finds installed open source software.
+REM    Copyright (C) 2007 OpenLogic, Inc.
+REM  
+REM OSS Discovery is free software: you can redistribute it and/or modify
+REM it under the terms of the GNU Affero General Public License version 3 as 
+REM published by the Free Software Foundation.  
+REM  
+REM 
+REM This program is distributed in the hope that it will be useful,
+REM but WITHOUT ANY WARRANTY; without even the implied warranty of
+REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+REM GNU Affero General Public License version 3 (discovery2-client/license/OSSDiscoveryLicense.txt) 
+REM for more details.
+REM  
+REM You should have received a copy of the GNU Affero General Public License along with this program.  
+REM If not, see http://www.gnu.org/licenses/
+REM  
+REM You can learn more about OSSDiscovery, report bugs and get the latest versions at www.ossdiscovery.org.
+REM You can contact the OSS Discovery team at info@ossdiscovery.org.
+REM You can contact OpenLogic at info@openlogic.com.
+
+set OSSDISCOVERY_HOME=%~dp0%
+set JAVA_HOME=%OSSDISCOVERY_HOME%\jre-1.5.0_07-windows-ia32\
+
+cd "%OSSDISCOVERY_HOME%"
+%OSSDISCOVERY_HOME\jruby\bin\jruby.bat %OSSDISCOVERY_HOME%\lib\discovery.rb --progress 100 --human-results readable_scanresults.txt %1 %2 %3 %4 %5 %6
+
