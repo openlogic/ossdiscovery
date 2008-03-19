880df7c9d3f18eea715597e8378a0626e020105e
Brian Noll <brian.noll@openlogic.com>
2008-03-19 17:53:44 +0000

    the 'jruby' dir now contains version 1.1RC3
    git-svn-id: svn://svn.code.sf.net/p/ossdiscovery/code/trunk/app@387 fd6d9157-2c40-0410-9642-e7077f30ef04

--- main/jruby/bin/jruby.bat
+++ main/jruby/bin/jruby.bat
@@ -7,9 +7,13 @@ rem for info on environment variables, see internal batch script _jrubyvars.bat
 setlocal
 call "%~dp0_jrubyvars" %*
 
-"%_STARTJAVA%" %_VM_OPTS% -cp "%CLASSPATH%" -Djruby.base="%JRUBY_BASE%" -Djruby.home="%JRUBY_HOME%" -Djruby.lib="%JRUBY_HOME%\lib" -Djruby.shell="cmd.exe" -Djruby.script=jruby.bat org.jruby.Main %JRUBY_OPTS% %_RUBY_OPTS%
+"%_STARTJAVA%" %_VM_OPTS% -cp "%CLASSPATH%" -Djruby.home="%JRUBY_HOME%" -Djruby.lib="%JRUBY_HOME%\lib" -Djruby.shell="cmd.exe" -Djruby.script=jruby.bat org.jruby.Main %JRUBY_OPTS% %_RUBY_OPTS%
 set E=%ERRORLEVEL%
 
 call "%~dp0_jrubycleanup"
-rem exit must be on the same line in order to see local %E% variable!
-endlocal & exit /b %E%
+
+rem 1. exit must be on the same line in order to see local %E% variable!
+rem 2. we must use cmd /c in order for the exit code properly returned!
+rem    See JRUBY-2094 for more details.
+endlocal & cmd /c exit /b %E%
+
