28f698ed1149f6a92d12750f311f39f7bc494d4b
Landon Cox <landon.cox@openlogic.com>
2008-03-21 19:41:32 +0000

    modified copyrights to include 2008
    git-svn-id: svn://svn.code.sf.net/p/ossdiscovery/code/trunk@394 fd6d9157-2c40-0410-9642-e7077f30ef04

--- app/main/lib/plugins/census/init.rb
+++ app/main/lib/plugins/census/init.rb
@@ -1,3 +1,30 @@
+# init.rb
+#
+# LEGAL NOTICE
+# -------------
+# 
+# OSS Discovery is a tool that finds installed open source software.
+#    Copyright (C) 2007-2008 OpenLogic, Inc.
+#  
+# OSS Discovery is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License version 3 as 
+# published by the Free Software Foundation.  
+#  
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License version 3 (discovery2-client/license/OSSDiscoveryLicense.txt) 
+# for more details.
+#  
+# You should have received a copy of the GNU Affero General Public License along with this program.  
+# If not, see http://www.gnu.org/licenses/
+#  
+# You can learn more about OSSDiscovery or contact the team at www.ossdiscovery.com.
+# You can contact OpenLogic at info@openlogic.com.
+#
+#--------------------------------------------------------------------------------------------------
+
 $:.unshift File.dirname(__FILE__)
 require 'conf/census_config'
 enabled = CensusConfig.census_enabled
