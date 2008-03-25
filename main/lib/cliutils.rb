483856f9ff26582132225e348dc1f123e6df0520
Landon Cox <landon.cox@openlogic.com>
2008-03-25 16:47:52 +0000

    fixed issue of inconsistent machine id generation when run on ruby vs jruby. issue was a regexp that evaluated different under jruby given the same source text.  found a workaround by modifying the regexp so it gives the same results in both cases
    tested under OS X which displayed the issue - all linux and os X platforms follow same code path and displayed same issue.  need to test under linux distros and insure the fix is working there as well.
    
    
    git-svn-id: svn://svn.code.sf.net/p/ossdiscovery/code/trunk/app@396 fd6d9157-2c40-0410-9642-e7077f30ef04

--- main/lib/cliutils.rb
+++ main/lib/cliutils.rb
@@ -689,10 +689,6 @@ end
 =end
 def make_simple_machine_id
 
-  if ( @kernel == nil )
-    @kernel = RbConfig::CONFIG["target_alias"] 
-  end
-
   hostname = Socket.gethostname
   ipaddr = IPSocket.getaddress(hostname)
   
@@ -744,7 +740,7 @@ def make_uname_based_machine_id(platform)
   # these are two forms of ifconfig output for showing mac, 'HWaddr' and 'ether'
   if ( ifconfig != nil && 
       ((matchdata = ifconfig.match( '(HWaddr) ([0-9:A-F].*?)+$')) != nil || 
-       (matchdata = ifconfig.match( '(ether) ([0-9:A-F].*?)+$' )) != nil) )
+       (matchdata = ifconfig.match( '(ether) ([0-9:A-F].*?)$' )) != nil) )
 
     # load the found mac address
     mac = matchdata[2]
@@ -771,6 +767,10 @@ def make_uname_based_machine_id(platform)
   # obfuscate the entire blob of info we gather into a single, 32 byte MD5 sum   
   # if any single one of these items changes over time, the machine ID will change.  This is a known
   # limitation.
+
+  puts "machine id blob:"
+  puts hostname + mac + @uname + @distro
+
   @machine_id = Digest::MD5.hexdigest(hostname + mac + @uname + @distro)
 end
 
