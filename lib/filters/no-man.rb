728f7d4ddd99d207da2bfe0696f29f3a0533b714
Landon Cox <landon.cox@openlogic.com>
2008-05-12 17:00:03 +0000

    added automatic exclusions for man pages and ubuntu dpkg/info directories.  these are always artifacts of installations, not binaries, but often trip up the rule engine because files in these directories are often named similar to the binary of the package that's installed.
    git-svn-id: svn://svn.code.sf.net/p/ossdiscovery/code/trunk/app/main@523 fd6d9157-2c40-0410-9642-e7077f30ef04

--- lib/filters/no-man.rb
+++ lib/filters/no-man.rb
@@ -1,7 +1,4 @@
-
-
-
-# generic-exclusions.rb
+# no-man.rb
 #
 # LEGAL NOTICE
 # -------------
@@ -31,13 +28,9 @@
 # --------------------------------------------------------------------------------------------------
 #
 
-# exclusions
+# a filter is defined by its unique description as the key value to a filter hash table
+# the value is simply a regular expression or literal filename or basename of a directory to ignore
 
-require "#{@filterdir}/no-hidden.rb"
-require "#{@filterdir}/no-tmp.rb"
-require "#{@filterdir}/no-system.rb"
-require "#{@filterdir}/no-pkgs.rb"
-require "#{@filterdir}/no-man.rb"
+# these files are not binaries, but artifacts of installations
 
-#require "#{@filterdir}/no-media.rb"
-
+@dir_exclusion_filters["No man directories"] = '^/usr/share/man'
