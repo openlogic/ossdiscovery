51dfb94d875e4b45c4e8f68eae529fea751c29e7
Landon Cox <landon.cox@openlogic.com>
2008-05-21 22:16:54 +0000

    refactoring
    git-svn-id: svn://svn.code.sf.net/p/ossdiscovery/code/trunk/app/main@566 fd6d9157-2c40-0410-9642-e7077f30ef04

--- lib/plugins/census/init.rb
+++ lib/plugins/census/init.rb
@@ -28,7 +28,7 @@
 $:.unshift File.dirname(__FILE__)
 require 'conf/census_config'
 enabled = CensusConfig.census_enabled
-require 'census_utils' if enabled
+require 'census_plugin' if enabled
 
 CENSUS_PLUGIN_VERSION = "1.0"
 CENSUS_PLUGIN_VERSION_KEY = "29the23special46secret31".to_i(36).to_s(16)
