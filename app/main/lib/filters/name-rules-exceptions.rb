3508c02e693767051982f5f3b3eefcddc95125af
Brad Reeves <brad.reeves@openlogic.com>
2009-01-28 22:35:50 +0000

    More Filters
    git-svn-id: svn://svn.code.sf.net/p/ossdiscovery/code/trunk@654 fd6d9157-2c40-0410-9642-e7077f30ef04

--- app/main/lib/filters/name-rules-exceptions.rb
+++ app/main/lib/filters/name-rules-exceptions.rb
@@ -43,3 +43,8 @@
 @file_exclusion_filters["Name Rules Exceptions html"] = '\.htm.?$'
 @file_exclusion_filters["Name Rules Exceptions autorun"] = '^autorun\..*$'
 @file_exclusion_filters["Name Rules Exceptions manpages"] = '\.[0-9]$'
+@file_exclusion_filters["Name Rules Exceptions template"] = '^template\..*$'
+@file_exclusion_filters["Name Rules Exceptions snippet"] = '^snippet\..*$'
+@file_exclusion_filters["Name Rules Exceptions style"] = '^style\..*$'
+@file_exclusion_filters["Name Rules Exceptions spell"] = '^spell\..*$'
+@file_exclusion_filters["Name Rules Exceptions test"] = '^test\..*$'
