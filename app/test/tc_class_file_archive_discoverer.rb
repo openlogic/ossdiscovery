5805bf0604ad05cea55e2af735f4ae97db08fd82
Rod Cope <rod.cope@openlogic.com>
2009-04-02 23:40:42 +0000

    Fixed broken tests related to simple API change.
    git-svn-id: svn://svn.code.sf.net/p/ossdiscovery/code/trunk@735 fd6d9157-2c40-0410-9642-e7077f30ef04

--- app/test/tc_class_file_archive_discoverer.rb
+++ app/test/tc_class_file_archive_discoverer.rb
@@ -14,34 +14,30 @@ class TcClassFileArchiveDiscoverer < Test::Unit::TestCase
   end
   
   def test_load_simple_jar_file
-    @disco = ClassFileArchiveDiscoverer.new(simple_test_jar_file_name)
-    paths = @disco.get_class_file_paths
+    paths = ClassFileArchiveDiscoverer.get_class_file_paths(simple_test_jar_file_name)
     assert_equal 1, paths.size
     assert_equal "org/apache/commons/collections/thing.class", paths.first
   end
 
   def test_discover_simple_jar_file
-    @disco = ClassFileArchiveDiscoverer.new(simple_test_jar_file_name)
     @tst.load_from_file(simple_test_file_name)
-    matches = @disco.discover
+    matches = ClassFileArchiveDiscoverer.discover(simple_test_jar_file_name)
     assert_equal 1, matches.size
     assert_equal "commons-collections", matches.keys.first
     assert_equal "org/apache/commons/collections/thing.class", matches.values.first
   end
 
   def test_discover_collections_jar_files
-    @disco = ClassFileArchiveDiscoverer.new(collections_jar_file_name)
     @tst.load_from_file(simple_test_file_name)
-    matches = @disco.discover
+    matches = ClassFileArchiveDiscoverer.discover(collections_jar_file_name)
     assert_equal 1, matches.size
     assert_equal "commons-collections", matches.keys.first
     assert_equal "org/apache/commons/collections/ArrayStack.class", matches.values.first
   end
 
   def test_discover_collections_jar_files_loaded_through_search_trees
-    @disco = ClassFileArchiveDiscoverer.new(collections_jar_file_name)
     SearchTrees.load_from_file(simple_test_file_name)
-    matches = @disco.discover
+    matches = ClassFileArchiveDiscoverer.discover(collections_jar_file_name)
     assert_equal 1, matches.size
     assert_equal "commons-collections", matches.keys.first
     assert_equal "org/apache/commons/collections/ArrayStack.class", matches.values.first
